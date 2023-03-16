57441da51f2a80864bd0b0d35ba4548f31f252db
ygeyzel <ygeyzel@gmail.com>
2023-03-17 01:36:58 +0200

    load images

--- utils/common.py
+++ utils/common.py
@@ -1,5 +1,6 @@
-from os.path import join
+import os
 
-RES_DIR = "res"
-IMAGES_DIR = join(RES_DIR, "images")
-SOUNDS_DIR = join(RES_DIR, "sounds")
+BASE_DIR = os.path.split(os.path.abspath(__file__))[0]
+RES_DIR = os.path.join(BASE_DIR, "res")
+IMAGES_DIR = os.path.join(RES_DIR, "images")
+SOUNDS_DIR = os.path.join(RES_DIR, "sounds")
