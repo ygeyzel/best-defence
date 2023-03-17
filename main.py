f459fa7e0844d29198b9f0a35d73db48a4840adf
ygeyzel <ygeyzel@gmail.com>
2023-03-17 13:51:42 +0200

    Road types, tiles

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame as pg
-from sprites.tile import Tile, TILE_WIDTH
+from utils.common import TILE_WIDTH
+from sprites.tile import Tile, TileObject
 
 
 SCREEN_WIDTH = 800
@@ -8,19 +9,25 @@ FPS = 60
 
 GRID_START_POS = (150, 200)
 INIT_GRID = (
-        (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0), 
-        (0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0) 
+    (None, None, None, None, None, None, None, None, None, None,
+     None, None, None, None, None, None, None, None, None, None),
+    (TileObject.REGULAR_ROAD, TileObject.REGULAR_ROAD, TileObject.REGULAR_ROAD, TileObject.REGULAR_ROAD, TileObject.REGULAR_ROAD, TileObject.REGULAR_ROAD, TileObject.REGULAR_ROAD, TileObject.REGULAR_ROAD, TileObject.REGULAR_ROAD, TileObject.ENGINEERED_ROAD,
+     TileObject.REGULAR_ROAD, TileObject.REGULAR_ROAD, TileObject.REGULAR_ROAD, TileObject.HARD_ROAD, TileObject.HARD_ROAD, TileObject.REGULAR_ROAD, TileObject.REGULAR_ROAD, TileObject.HARD_ROAD, TileObject.REGULAR_ROAD, TileObject.REGULAR_ROAD)
 )
 
 
-def init_tiles(groups):
+def init_tiles(group):
     for i, grid_row in enumerate(INIT_GRID):
-        for j, block_level in enumerate(grid_row):
+        for j, tile_object in enumerate(grid_row):
             x = GRID_START_POS[0] + TILE_WIDTH * j
             y = GRID_START_POS[1] + TILE_WIDTH * i
-            tile = Tile(block_level, (x, y))
-            for group in groups:
-                group.add(tile)
+            tile = Tile((x, y))
+
+            group.add(tile)
+
+            if tile_object is not None:
+                populated_obj = tile.populate_tile(tile_object)
+                group.add(populated_obj)
 
 
 def main():
@@ -30,10 +37,9 @@ def main():
     clock = pg.time.Clock()
     running = True
 
-    tiles = pg.sprite.Group()
     all_sprites = pg.sprite.RenderPlain()
 
-    init_tiles((tiles, all_sprites))
+    init_tiles(all_sprites)
 
     while running:
         for event in pg.event.get():
