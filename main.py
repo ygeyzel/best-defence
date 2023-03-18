f9ce920a216a0217c565c9675d330ed8187f405d
Yoav Kostiner <yoavko204@gmail.com>
2023-03-18 02:01:03 +0200

    clean up main

--- main.py
+++ main.py
@@ -1,44 +1,13 @@
 import pygame as pg
 
 from gameplay.general_fire_management import towers_fire_management
-from utils.common import TILE_WIDTH
 from gameplay.movement import set_soldiers_manuverability
-from sprites.tile import Tile, TileObject
-from sprites.roads import Road
-from sprites.tower import Tower
 from sprites.soldier import Soldier
-from utils import grid
+from utils.grid import init_tiles_groups
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 400
 FPS = 60
-
-
-def init_tiles_groups():
-    all_sprites = pg.sprite.RenderPlain()
-    roads = pg.sprite.Group()
-    towers = pg.sprite.Group()
-    GRID_START_POS, INIT_GRID = grid.load_map_from_file('docs/test_level.json')
-
-    for i, grid_row in enumerate(INIT_GRID):
-        for j, tile_object in enumerate(grid_row):
-            x = GRID_START_POS[0] + TILE_WIDTH * j
-            y = GRID_START_POS[1] + TILE_WIDTH * i
-            tile = Tile((x, y))
-
-            all_sprites.add(tile)
-
-            if tile_object is not TileObject.EMPTY:
-                populated_obj = tile.populate_tile(tile_object)
-                all_sprites.add(populated_obj)
-
-                if isinstance(populated_obj, Road):
-                    roads.add(populated_obj)
-
-                if isinstance(populated_obj, Tower):
-                    towers.add(populated_obj)
-
-    return all_sprites, roads, towers
 
 
 def main():
@@ -49,13 +18,13 @@ def main():
     clock = pg.time.Clock()
     running = True
 
-    all_sprites, roads, towers = init_tiles_groups()
+    all_sprites, roads, towers = init_tiles_groups('docs/test_level.json')
     soldiers = pg.sprite.Group()
 
 
-    # soldier_0 = Soldier((150, 240), 200, 2, 50) # temp
-    # soldier_0.start_movement()
-    # all_sprites.add(soldier_0)
+    soldier_0 = Soldier((150, 240), 200, 2, 50) # temp
+    soldier_0.start_movement()
+    all_sprites.add(soldier_0)
 
     while running:
         for event in pg.event.get():
--- utils/grid.py
+++ utils/grid.py
@@ -1,8 +1,13 @@
 import json
 import pygame as pg
 from sprites.tile import Tile, TileObject
+from utils.common import TILE_WIDTH
+from sprites.tile import Tile, TileObject
+from sprites.roads import Road
+from sprites.tower import Tower
 
-def load_map_from_file(map_file_path: str, tile_set='docs/tile_set.json'): #grop, 
+
+def load_map_from_file(map_file_path: str, tile_set='docs/tile_set.json'): #grop,
     with open(tile_set, 'r') as f:
         tiles_set = json.load(f)
 
@@ -15,3 +20,32 @@ def load_map_from_file(map_file_path: str, tile_set='docs/tile_set.json'): #grop
             grid.append(row)
 
     return grid_start_pos, grid
+
+
+def init_tiles_groups(level_file_path: str):
+    all_sprites = pg.sprite.RenderPlain()
+    roads = pg.sprite.Group()
+    towers = pg.sprite.Group()
+    GRID_START_POS, INIT_GRID = load_map_from_file(level_file_path)
+
+    for i, grid_row in enumerate(INIT_GRID):
+        for j, tile_object in enumerate(grid_row):
+            x = GRID_START_POS[0] + TILE_WIDTH * j
+            y = GRID_START_POS[1] + TILE_WIDTH * i
+            tile = Tile((x, y))
+
+            all_sprites.add(tile)
+
+            if tile_object is not TileObject.EMPTY:
+                populated_obj = tile.populate_tile(tile_object)
+                all_sprites.add(populated_obj)
+
+                if isinstance(populated_obj, Road):
+                    roads.add(populated_obj)
+
+                if isinstance(populated_obj, Tower):
+                    towers.add(populated_obj)
+
+    return all_sprites, roads, towers
+
+
